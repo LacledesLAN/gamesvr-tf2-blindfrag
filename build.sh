c3d0c6c7bd291ef1d6ef3b54611e9c6af1e4d3c3
James Dudley <jedudley@outlook.com>
2023-04-29 14:26:36 -0500

    updates; still broken :(

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -e
+set -e;
+set -u;
 
 echo -e '\n\033[1m[Build tf2:blindfrag]\033[0m'
-docker build . -f linux.Dockerfile --rm -t lacledeslan/gamesvr-tf2-blindfrag:latest --build-arg BUILDNODE="$(cat /proc/sys/kernel/hostname)";
+docker build . --pull -f linux.Dockerfile --rm -t lacledeslan/gamesvr-tf2-blindfrag:latest --build-arg BUILDNODE="$(cat /proc/sys/kernel/hostname)";
 docker run -it --rm lacledeslan/gamesvr-tf2-blindfrag:latest ./ll-tests/gamesvr-tf2-blindfrag.sh;
 docker push lacledeslan/gamesvr-tf2-blindfrag:latest
